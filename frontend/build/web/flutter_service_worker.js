09e81cab2fab55c3b0955c56764c1e3d6a0a5fbc
GitHub Action <action@github.com>
2023-09-22 08:07:50 +0000

    Update frontend build

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "ffb2bb6484d5689d91f393b60
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "d20b0d265f5694583836ab14551446fd",
-"/": "d20b0d265f5694583836ab14551446fd",
+"index.html": "e9093920e4bac8afd0402ec7bca0cc1b",
+"/": "e9093920e4bac8afd0402ec7bca0cc1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "e0de352d73159b0e59fcdb06386e2f7e",
 "manifest.json": "0fa552613b8ec0fda5cda565914e3b16"};
